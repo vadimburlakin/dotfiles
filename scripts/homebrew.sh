c2a066aabf23e3a263f62c20d07e4bc885c4e999
mt-vadim <vburlakin@moneytree.jp>
2022-01-26 14:40:34 +0900

    iterate

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
+CONFIGFILENAME=".zshrc.homebrew"
+CONFIGFILE="$HOME/$CONFIGFILENAME"
+SCRIPT="homebrew"
+BREWFORMULA="homebrew"
+
+while getopts r flag
+do
+  case "${flag}" in
+    r) REINSTALL="true";;
+  esac
+done
+REINSTALL=${REINSTALL:-false}
+
+undo () {
+  brew remove $BREWFORMULA
+  rm -rf $CONFIGFILE
+  sed -i '' '/homebrew/d' ~/.zshrc
+}
+
+if [ "$REINSTALL" == "true" ]; then
+  warning "Reinstalling"
+  undo
+fi
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if [[ -e $CONFIGFILE ]]; then
+  warning "$CONFIGFILE already exists, renaming it to $CONFIGFILE.backup"
+  mv $CONFIGFILE $CONFIGFILE.backup
+fi
+
+cp -R $SCRIPTSDIR/$SCRIPT/$CONFIGFILENAME $CONFIGFILE
+echo "source ~/.zshrc.rbenv" >> ~/.zshrc
+
+source $CONFIGFILE
